c7c19eb16029c27e5839312ece5c719328359215
Martijn Harthoorn <m.harthoorn@furore.com>
2018-09-02 23:15:52 +0200

    move to netstandard 2.0 and net4.72 - move code to new ImageProcessing library

--- Application/MainForm.cs
+++ Application/MainForm.cs
@@ -6,9 +6,9 @@ using System.IO;
 
 namespace ImageApp
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
             InitPreview(@"..\..\..\SampleImages");
--- Application/Program.cs
+++ Application/Program.cs
@@ -16,7 +16,7 @@ namespace ImageApp.Understanding
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
